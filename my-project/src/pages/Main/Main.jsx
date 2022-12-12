5aa4be7d7c45092b0ea55db0fd1f32e43884a0d3
minchan <chamin966@naver.com>
2022-12-13 00:14:30 +0900

    Design: 메인 페이지 배너, 설문 테이블, 페이지네이션 완성

--- my-project/src/pages/Main/Main.jsx
+++ my-project/src/pages/Main/Main.jsx
@@ -5,7 +5,117 @@ function Main() {
   useEffect(() => {});
   return (
     <>
-      <main className='main'>Main</main>
+      <div className='main-upper'>
+        <div className='main-upper__img'>
+          <img src='/images/samples/sample2.jpg' alt='배너이미지' height={'300px'} width={'100%'} />
+        </div>
+        <div className='main-upper__bg'>sadsadsadsadsa</div>
+        <div className='main-upper__text'>
+          <p>당신이 원하는 설문을 찾아보세요.</p>
+        </div>
+      </div>
+      <main className='main-page'>
+        <div className='main-filter'></div>
+        <div className='main-page__surveys'>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          {/* 샘플용 시작*/}
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>
+              2023 대학생 만족도 설문조사023 대학생 만족도 설문조사023 대학생 만족도 설문조사2023 대학생 만족도
+            </li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>
+              2023 대학생 만족도 설문조사023 대학생 만족도 설문조사023 대학생 만족도 설문조사2023 대학생 만족도
+            </li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>
+              2023 대학생 만족도 설문조사023 대학생 만족도 설문조사023 대학생 만족도 설문조사2023 대학생 만족도
+            </li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>
+              2023 대학생 만족도 설문조사023 대학생 만족도 설문조사023 대학생 만족도 설문조사2023 대학생 만족도
+            </li>
+          </ul>
+          <ul className='survey'>
+            <li>
+              <img src='/images/samples/sample1.png' alt='설문조사 썸네일' height={'170px'} width={'290px'} />
+            </li>
+            <li className='survey__title'>2023 대학생 만족도 설문조사</li>
+          </ul>
+          {/* 샘플용 끝*/}
+        </div>
+        {/* 페이지네이션 */}
+        <ul className='pagenation'>
+          <li className='pagenation__chevron'>{'<'}</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+          <li className='pagenation__chevron'>{'>'}</li>
+        </ul>
+      </main>
     </>
   );
 }
